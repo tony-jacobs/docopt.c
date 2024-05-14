a2e710ca4adfada73fb16c5447908f48f82d3037
Tony Jacobs <tony@tonyjacobsconsulting.com>
2024-05-14 11:04:52 -0400

    Bump for tj4

--- docopt_c.py
+++ docopt_c.py
@@ -26,7 +26,7 @@ Arguments:
 """
 
 __author__ = "Vladimir Keleshev"
-__version__ = "2.0rc2tj3"
+__version__ = "2.0rc2tj4"
 __description__ = "C generator for language for description of command-line interfaces"
 
 import numbers
